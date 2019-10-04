5543c7b66dc6ee0018b354e46bd3dd0a3ffe1a90
Vinit Goyal <vinitg@opentext.com>
2019-10-04 18:17:00 +0530

    third commit

---
+++ src/test/java/com/bn/opentext/SampleTest.java
@@ -0,0 +1,26 @@
+package com.bn.opentext;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+
+    @Test
+    public void test1None() {
+        System.out.println("inside SampleTest->test1, Neither smoke nor regression.");
+    }
+
+    @Test(description = "smoke test cases", groups = { "smoke" })
+    public void test2Smoke() {
+        System.out.println("inside SampleTest->test2, Smoke only");
+    }
+
+    @Test(description = "regression testcases", groups = { "regression" })
+    public void test3Regression() {
+        System.out.println("inside SampleTest->test3, Regression only");
+    }
+
+    @Test(description = "smoke and regression both", groups = { "smoke", "regression" })
+    public void test4SmokeRegression() {
+        System.out.println("inside SampleTest->test4, Smoke and Regression both");
+    }
+}
