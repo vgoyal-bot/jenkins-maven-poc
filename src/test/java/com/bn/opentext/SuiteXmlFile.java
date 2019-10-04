fe8cba844190900417aedaa008e6ee2a61f8f9bd
Vinit Goyal <vinitg@opentext.com>
2019-10-04 23:20:29 +0530

    Updated file

---
+++ src/test/java/com/bn/opentext/SuiteXmlFile.java
@@ -0,0 +1,11 @@
+package com.bn.opentext;
+
+import org.testng.annotations.Test;
+
+public class SuiteXmlFile {
+
+    @Test
+    public void test1None() {
+        System.out.println("inside SuiteXmlFile->test1, Neither smoke nor regression.");
+    }
+}
